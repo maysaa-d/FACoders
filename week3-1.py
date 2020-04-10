93f3eea1bc41bf69e6d1751651314cbbfcee70e0
maysaa-d <maysaa.w.daher@gmail.com>
2020-04-11 02:49:42 +0300

    3rd mission

---
+++ week3-1.py
@@ -0,0 +1,7 @@
+print('Numbers from 1 to 10')
+Guess= int(input("Guess the number:"))
+number = 2
+while Guess != number:
+     Guess = int(input("Guess the number:"))
+if Guess == 2 :
+    print("Great! You did it!")
